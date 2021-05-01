45318032cdb626b0daf7c19a32fc93739c6bd717
Aline Soares da Silva <aline.as385@gmail.com>
2021-05-01 00:12:14 -0300

    first commit

---
+++ src/main/java/com/fatec/scelwebdriver/Cap09ScelApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.scelwebdriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cap09ScelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cap09ScelApplication.class, args);
+	}
+
+}
